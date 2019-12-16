ad607dfc5b0d945f184e5a5591775220c903f99e
Reach <sovanreach.c007@gmail.com>
2019-12-17 05:33:15 +0800

    improve security with dotenv

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -7,6 +8,8 @@ const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
 
 const app = express();
+
+console.log(process.env.API_KEY);
 
 
 app.use(express.static('public'));
@@ -21,8 +24,7 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret , encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ["password"] });
 
 
 const User = new mongoose.model("User", userSchema);
