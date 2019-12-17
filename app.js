817b3a8e615fe1ba12bbd4a87ed1dcbe3a0b2227
Reach <sovanreach.c007@gmail.com>
2019-12-17 13:46:21 +0800

    Security level 3 - add hashing algorithm

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
+
 
 const app = express();
 
-console.log(process.env.API_KEY);
+// console.log(process.env.API_KEY);
 
 
 app.use(express.static('public'));
@@ -23,8 +24,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ["password"] });
 
 
 const User = new mongoose.model("User", userSchema);
@@ -43,7 +42,7 @@ app.get("/login", function(req, res) {
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser) {
     if(err){
@@ -65,7 +64,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err) {
