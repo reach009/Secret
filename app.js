da0d4a82b3cda27e24aff4d267b415ea4ef5e89b
Reach <sovanreach.c007@gmail.com>
2019-12-19 14:22:25 +0800

    add secrets functionality, the user can now submit their secrets annynomously.

--- app.js
+++ app.js
@@ -43,7 +43,8 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   googleId: String,    // google id
-  facebookId: String    // facebook id
+  facebookId: String,    // facebook id
+  secret: String
 });
 
 // add passport plugin to mongoose
@@ -170,13 +171,47 @@ app.post("/register", function(req, res) {
 
 });
 
-app.get("/secrets", function(req, res){
+app.get("/submit", function(req, res){
   if (req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
 });
+
+
+app.post("/submit", function(req, res) {
+  const submittedSecret = req.body.secret;
+
+  console.log(req.user.id);
+
+  User.findById(req.user.id, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function() {
+          res.redirect("/secrets");
+        })
+      }
+    }
+  });
+
+});
+
+app.get("/secrets", function(req, res){
+  User.find({"secret": {$ne: null}}, function(err, foundUsers){
+    if(err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", {usersWithSecrets : foundUsers});
+      }
+    }
+  });
+});
+
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
